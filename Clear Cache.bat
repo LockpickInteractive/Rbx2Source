fcf75b52076b09bbf9f4c97b6ada767367ce4552
qfoxb <42038239+qfoxb@users.noreply.github.com>
2022-09-15 11:42:40 -0500

    spellin

--- Clear Cache.bat
+++ Clear Cache.bat
@@ -1,5 +1,17 @@
 @echo off
 color 7c
 echo This program will automatically remove the Rbx2Source Cache folder.
+:choice
+set /P c=Would you like to clear the Rbx2Source Cache folder now [Y/N]?
+if /I "%c%" EQU "Y" goto :execute
+if /I "%c%" EQU "N" goto :end
+goto :choice
+
+
+:execute
 pause
 rd %LOCALAPPDATA%\Rbx2Source /s
+
+
+
+:end
