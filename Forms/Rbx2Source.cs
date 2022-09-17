e0b6a5dba08887a54580f56a391ab1cfefeee963
qfoxb <42038239+qfoxb@users.noreply.github.com>
2022-09-16 23:27:42 -0500

    Revert "remove comment, change thumbnail url and remove useless function now (will be rewritten for new format)"
    This reverts commit c9661622d1715ff8b75f2a4ac0641e64c37e0c47.

--- Forms/Rbx2Source.cs
+++ Forms/Rbx2Source.cs
@@ -254,6 +254,7 @@ namespace Rbx2Source
 
         private void gatherSourceGames(string steamDir)
         {
+            //string steamPath = Path.Combine(steamDir, "steamapps", "common");
             string configDir = ConfigLoader();
             string steamPath = Path.Combine(configDir, "steamapps", "common");
             if (Directory.Exists(steamPath))
@@ -344,8 +345,8 @@ namespace Rbx2Source
 
             if (compilerTypeSelect.Text == "Avatar")
             {
-                assetPreviewImage = "https://www.roblox.com/headshot-thumbnail/image?userId=" + currentUser.Id + "&width=420&height=420&format=png";
-                compilerInput.Text = "Username:"; 
+                assetPreviewImage = "https://www.roblox.com/headshot-thumbnail/json?width=420&height=420&format=png&userId=" + currentUser.Id;
+                compilerInput.Text = "Username:";
                 compilerInputField.Text = currentUser.Username;
                 compilerTypeIcon.Image = Properties.Resources.Humanoid_icon;
             }
@@ -814,13 +815,12 @@ namespace Rbx2Source
                 {
                     if (assetPreview.ImageLocation != assetPreviewImage)
                     {
-                       // CdnPender check = WebUtility.DownloadJSON<CdnPender>(assetPreviewImage);
+                        CdnPender check = WebUtility.DownloadJSON<CdnPender>(assetPreviewImage);
 
-                        if (true)
+                        if (check.Final)
                         {
-                          //  assetPreviewImage = check.Url;
-                          //  assetPreview.ImageLocation = check.Url;
-                          
+                            assetPreviewImage = check.Url;
+                            assetPreview.ImageLocation = check.Url;
                         }
                         else
                         {
